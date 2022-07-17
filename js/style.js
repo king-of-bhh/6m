a57a94f73f36923a8853754ad7ce98ddbaa4700c
king_of_bhh <102011655+king-of-bhh@users.noreply.github.com>
2022-07-17 09:36:29 +0600

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,9 +1,1 @@
-var myVar;
 
-function time() {
-  myVar = setTimeout(showPage, 5000);
-}
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("text").style.display = "block";
-}
